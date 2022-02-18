ff2e0607ff4c634e36318bff2386b4f733cba3fa
wangxin <xinwang213z@gmail.com>
2022-02-18 17:50:17 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,7 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
